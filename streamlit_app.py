32b232dfb7ce77639192cf7a7c9dcf91f00956d7
SP_DataScienceProjects <stevenewsblog123@gmail.com>
2025-02-21 11:18:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.title("hello world")
 
-st.write('Hello world!')
+st.header("This is a header with a divider", divider='rainbow')
+
+st.markdown("Welcome to our app where you can get free advice")
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.area_chart(chart_data)
